b4cda28652846208389e8b546eb577ac6dfeb0c4
Amit29-hub <amukhram@albany.edu>
2026-05-06 16:59:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 function showMessage() {
     alert("Welcome to RoomSync! Start exploring compatible roommates.");
 }
+
+
+
+
+
+function filterRoommates() {
+
+    let input = document.getElementById("searchInput");
+
+    let filter = input.value.toLowerCase();
+
+    let cards = document.getElementsByClassName("roommate-card");
+
+    for (let i = 0; i < cards.length; i++) {
+
+        let text = cards[i].innerText.toLowerCase();
+
+        if (text.includes(filter)) {
+
+            cards[i].style.display = "";
+
+        } else {
+
+            cards[i].style.display = "none";
+        }
+    }
+}
