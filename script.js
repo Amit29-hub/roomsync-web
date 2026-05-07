919695b1f6d530e7356b94a06caf38f3ae9a8b39
Amit29-hub <amukhram@albany.edu>
2026-05-07 19:25:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -175,3 +175,29 @@ function openProfile(name, details) {
 function closeProfile() {
     document.getElementById("profileModal").style.display = "none";
 }
+
+
+
+function toggleDarkMode() {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+
+        localStorage.setItem("roomSyncDarkMode", "on");
+
+    } else {
+
+        localStorage.setItem("roomSyncDarkMode", "off");
+    }
+}
+
+function applyDarkMode() {
+
+    let darkMode = localStorage.getItem("roomSyncDarkMode");
+
+    if (darkMode === "on") {
+
+        document.body.classList.add("dark-mode");
+    }
+}
